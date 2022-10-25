5602e868aeb773c3f27a02bb63b728ec770edf40
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-10-25 22:29:24 +0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -124,7 +124,7 @@ def set_parameters():
   initial_balance = st.number_input("Initial account balance (THB):", min_value=0, step=1000, value=1000000)
   trading_size_pct = st.slider("Trading size as a percentage of initial account balance (%):", 0, 100, 10)
   trade_size = initial_balance * trading_size_pct / 100
-  st.write('{}% of initial investment is {:,} THB'.format(trading_size_pct, trade_size))
+  st.write('{}% of initial investment is {:,.0f} THB'.format(trading_size_pct, trade_size))
   commission_fee_pct = st.number_input("Commission fee (%):", min_value=0.000, step=0.001, value=0.157, format='%1.3f')
 
   set_param_button = st.button("Set Parameters")
