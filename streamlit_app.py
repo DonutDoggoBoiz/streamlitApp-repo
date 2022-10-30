e5c2ff99211c720f900ccb0748d6500d834d1ca7
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-10-30 22:18:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -120,8 +120,8 @@ if st.session_state['login_status'] == False:
       with placeholder2.container():
         register_form = st.form('Register')
         register_form.subheader('Register 📝')
-        new_name = register_form.text_input('Name', placeholder='eg. Warren Buffet')
-        new_username = register_form.text_input('Username', placeholder='your username')
+        new_name = register_form.text_input('Name', placeholder='eg. Michael Burry')
+        new_username = register_form.text_input('Username', placeholder='eg. mikeburr1971')
         new_password = register_form.text_input('Password', type='password', placeholder='your password')
         register_button = register_form.form_submit_button('Register')
         if register_button:
