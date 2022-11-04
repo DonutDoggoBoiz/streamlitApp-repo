c6a933cf4bdbc3e3c13b9a437d56c1339b752d0a
DonutDoggoBoiz <95366166+DonutDoggoBoiz@users.noreply.github.com>
2022-11-04 18:50:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -536,22 +536,22 @@ else:
                 set_param_button = st.form_submit_button("Set Parameters")
                 if set_param_button:
                   model_db.put({'username':st.session_state['username'],
-                               'model_name':agent_name,
-                               'stock_quote':stock_name,
-                               'start_date':start_date,
-                               'end_date':end_date,
-                               'split_point':split_point,
-                               'episode_trained':0,
-                               'trained_result':0,
-                               'test_result':0,
-                               'gamma':agent_gamma,
-                               'epsilon_start':agent_epsilon,
-                               'epsilon_decline':agent_epsilon_dec,
-                               'epsilon_min':agent_epsilon_end,
-                               'learning_rate':agent_lr,
-                               'initial_balance':initial_balance,
-                               'trading_size_pct':trading_size_pct,
-                               'commission_fee_pct':commission_fee_pct}
+                                'model_name':agent_name,
+                                'stock_quote':stock_name,
+                                'start_date':start_date,
+                                'end_date':end_date,
+                                'split_point':split_point,
+                                'episode_trained':0,
+                                'trained_result':0,
+                                'test_result':0,
+                                'gamma':agent_gamma,
+                                'epsilon_start':agent_epsilon,
+                                'epsilon_decline':agent_epsilon_dec,
+                                'epsilon_min':agent_epsilon_end,
+                                'learning_rate':agent_lr,
+                                'initial_balance':initial_balance,
+                                'trading_size_pct':trading_size_pct,
+                                'commission_fee_pct':commission_fee_pct}
                               )
                   st.success('Set parameters successful!  Please proceed to "Train Model" tab')
                   ####### -------- #######
